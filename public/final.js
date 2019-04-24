9367f4921edc6de959848013207538a1186f696c
dhtprbs12 <dhtprbs12@gmail.com>
2019-04-23 19:47:35 -0700

    first commit

---
+++ public/final.js
@@ -0,0 +1,147 @@
+/*
+Sekyun Oh
+CSC 337
+Final Project
+04/24/2019
+chatit.js
+
+This is a JavaScript code for the final web page's behavior.
+This JavaScript code interacts with final_service.js to get
+data from the server.
+*/
+
+'use strict';
+(function(){
+	/**
+	window.onload = function()
+
+	This is an anonymous function that is called when html file is being loaded.
+	**/
+	window.onload = function(){
+		getList();
+		document.getElementById('input').onkeyup = userTyping;
+	};
+
+	function getList(){
+		// call ajax
+		let url = 'http://localhost:3000/';
+		fetch(url)
+		.then(checkStatus)
+		.then(function(responseText) {
+			let res = JSON.parse(responseText);
+			let people = res['people'];
+			displayList(people)
+		})
+		.catch(function(error) {
+			// show error
+			displayError(error + ' while getting list');
+		});
+	}
+
+	function userTyping(event){
+		let value = document.getElementById('input').value;
+		let url = 'http://localhost:3000?name='+value;
+
+		// fetch voterUrl
+		fetch(url)
+		.then(checkStatus)
+		.then(function(responseText) {
+			let res = JSON.parse(responseText);
+			let people = res['people'];
+			displayList(people);
+		})
+		.catch(function(error) {
+			// error: do something with error
+			displayError(error + ' while displaying list');
+		});
+	}
+
+	function displayList(people){
+		document.getElementById('list').innerHTML = '';
+		let i;
+		for(i = 0; i < people.length; i++){
+			let person = people[i];
+			let div = document.createElement('div');
+			div.className = person['url'];
+			div.onclick = modal;
+			div.id = i.toString();
+			let img = document.createElement('img');
+			let infoDiv = document.createElement('div');
+			let h3 = document.createElement('h3');
+			let p = document.createElement('p');
+
+			let image = '../image/'+person['pic'].replace(/^\s+|\s+$/g,'');
+			let fullname = person['name'].split(' ');
+			let name = fullname[0] + ' ' + fullname[1];
+			let addr = person['address'].replace(/^\s+|\s+$/g,'');
+
+			img.src = image;
+			h3.innerHTML = name;
+			p.innerHTML = addr;
+			infoDiv.appendChild(h3);
+			infoDiv.appendChild(p);
+
+			div.appendChild(img);
+			div.appendChild(infoDiv);
+
+			document.getElementById('list').appendChild(div);
+		}
+	}
+
+	function displayError(error){
+		document.getElementById('list').innerHTML = '';
+		let h1 = document.createElement('h1');
+		h1.className = 'error';
+		h1.innerHTML = error;
+		document.getElementById('list').appendChild(h1);
+	}
+
+	function modal(){
+
+		document.getElementById('modal-pic').src = this.children[0].getAttribute('src');
+		document.getElementById('modal-name').innerHTML = '';
+		let a = document.createElement('a');
+		a.innerHTML = this.children[1].children[0].innerHTML;
+		a.href = this.getAttribute('class');
+		a.target = '_blank';
+		document.getElementById('modal-name').appendChild(a);
+
+		let url = 'http://localhost:3000/description?id='+this.getAttribute('id');
+		// fetch url
+		fetch(url)
+		.then(checkStatus)
+		.then(function(responseText) {
+			let res = JSON.parse(responseText);
+			document.getElementById('modal-desc').innerHTML = res['desc'];
+		})
+		.catch(function(error) {
+			// error: do something with error
+			//showError(error,);
+			displayError(error + ' while fetching description');
+		});
+
+		let modal = document.getElementById('myModal');
+		modal.style.display = "block";
+
+		document.getElementsByClassName("close")[0].onclick = function() {
+			modal.style.display = "none";
+		}
+	}
+
+	/**
+	checkStatus(response)
+
+	This function is called when Ajax fetchs url.
+	This function simply determines if the Ajax call was successful or not.
+	This is a main function that we can handle errors.
+	**/
+	function checkStatus(response) {
+
+		if (response.status >= 200 && response.status < 300) {
+			return response.text();
+		}
+		else{
+			return Promise.reject(new Error(response.status+":"+response.statusText));
+		}
+	}
+})();
---
+++ public/final_service.js
@@ -0,0 +1,80 @@
+/*
+Sekyun Oh
+CSC 337
+Final Project
+04/24/2019
+final_service.js
+
+This is a JavaScript code for the final web page's data.
+This JavaScript code interacts with final.js to send
+data to HTML.
+*/
+
+const express = require("express");
+const app = express();
+var fs = require('fs');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+app.use(express.static('public'));
+
+// main get comment function
+app.get('/',function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+
+	var value = req.query.name;
+	let arr = [];
+	let people = fs.readFileSync(__dirname+'/info.txt', 'utf8').split('\n');
+
+	if(value == undefined) {
+
+		for (let i = 0; i < people.length - 1; i++) {
+			let person = people[i].split(',');
+			let name = person[0];
+			let addr = person[1];
+			let pic = person[2];
+			let url = person[3];
+
+			let obj = {'name':name, 'address':addr, 'pic':pic, 'url':url};
+			arr.push(obj);
+		}
+
+	}else{
+
+		for (let i = 0; i < people.length - 1; i++) {
+			let person = people[i].split(',');
+			let name = person[0];
+			let addr = person[1];
+			let pic = person[2];
+			let url = person[3];
+
+			if(name.toLowerCase().includes(value)){
+				let obj = {'name':name, 'address':addr, 'pic':pic, 'url':url};
+				arr.push(obj);
+			}
+		}
+
+	}
+	let object = {'people':arr};
+	res.send(JSON.stringify(object));
+});
+
+app.get('/description', function(req,res){
+	res.header("Access-Control-Allow-Origin", "*");
+
+	var value = req.query.id;
+	let people = fs.readFileSync(__dirname+'/des.txt', 'utf8').split('\n\n');
+	let object = {'desc':people[value]};
+	res.send(JSON.stringify(object));
+})
+
+// listen connection on 3000 PORT
+app.listen(process.env.PORT, function(){
+	console.log('web service started on 3000 port!');
+});
