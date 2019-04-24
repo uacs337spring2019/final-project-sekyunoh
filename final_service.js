ead45c19729e4e64d4538eba16eed9586dbeec98
dhtprbs12 <dhtprbs12@gmail.com>
2019-04-23 21:16:11 -0700

    add render html function

--- final_service.js
+++ final_service.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static('public'));
+app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res){
