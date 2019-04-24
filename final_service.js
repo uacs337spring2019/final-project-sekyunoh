39f04e146317b9e76078055cd0c008a78b8ca3be
dhtprbs12 <dhtprbs12@gmail.com>
2019-04-23 21:24:10 -0700

    nineth commit

--- final_service.js
+++ final_service.js
@@ -24,7 +24,6 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static('public'));
-app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res){
